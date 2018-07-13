e18f5590d330ebce7b830891f339646f5836c871
Dennis Skoko <dennis.skoko@hotmail.com>
2018-07-13 17:39:44 +0200

    Added dotenv

--- server/conf/settings.ts
+++ server/conf/settings.ts
@@ -1,3 +1,13 @@
+import * as path from 'path'
+import * as dotenv from 'dotenv'
+
+// Will be the server folder after building
+const root = path.resolve(__dirname, '../..')
+
+dotenv.config({
+  path: path.join(root, '.env')
+})
+
 export const settings = {
-  port: 80
+  port: process.env.PORT || 80
 }
