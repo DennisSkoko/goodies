d49e37f5f1c0433463f25335161b1e5c5421bb27
Dennis Skoko <dennis.skoko@hotmail.com>
2018-07-15 10:56:14 +0200

    Added logging after connecting to the database

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,8 +1,18 @@
 import { ConnectionManager } from 'typeorm'
 
 import { settings } from '../../conf/settings'
+import { logger } from '../logger'
 
 const manager = new ConnectionManager()
 
 export const connection = manager.create(settings.database)
+
 connection.connect()
+  .then(() => {
+    logger.info('Successfully connected to the database')
+  })
+  .catch((err: Error) => {
+    logger.error('Failed to connect to the database', {
+      error: err.message
+    })
+  })
