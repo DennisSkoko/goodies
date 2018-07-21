41bb063c1c6031175c34634a0ad1fc10c6d120a5
Dennis Skoko <dennis.skoko@hotmail.com>
2018-07-21 15:21:25 +0200

    Added authentication route

--- server/src/hasher.ts
+++ server/src/hasher.ts
@@ -5,5 +5,9 @@ import { settings } from '../conf/settings'
 export const hasher = {
   hash (password: string) {
     return bcrypt.hash(password, settings.hasher.saltRounds)
+  },
+
+  compare (plain: string, password: string) {
+    return bcrypt.compare(plain, password)
   }
 }
---
+++ server/src/middlewares/routers/auth.ts
@@ -0,0 +1,59 @@
+import { Router, Request, Response, NextFunction } from 'express'
+
+import { connection as db } from '../../db/connection'
+import { User } from '../../db/entities'
+import { hasher } from '../../hasher'
+import { UnprocessableEntity } from '../../errors'
+import { jwt } from '../../jwt';
+
+export const auth = Router()
+
+auth.post('/api/auth', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    if (!req.body.password) {
+      next(new UnprocessableEntity([
+        {
+          property: 'password',
+          constraints: [
+            'password is required'
+          ]
+        }
+      ]))
+      return
+    }
+
+    const user = await db.manager.findOneOrFail(User, { email: req.body.email })
+    const passwordMatch = await hasher.compare(req.body.password, user.password)
+
+    if (!passwordMatch) {
+      next(new UnprocessableEntity([
+        {
+          property: 'password',
+          constraints: [
+            'password is incorrect'
+          ]
+        }
+      ]))
+      return
+    }
+
+    res.json({
+      id: user.id,
+      token: await jwt.sign({ sub: user.id })
+    })
+  } catch (err) {
+    if (err.name === 'EntityNotFound') {
+      next(new UnprocessableEntity([
+        {
+          property: 'email',
+          constraints: [
+            'email is unregistered'
+          ]
+        }
+      ]))
+      return
+    }
+
+    next(err)
+  }
+})
