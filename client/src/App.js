31ab7e195333c524f2dc846a3e272e2e608ccfda
Dennis Skoko <dennis.skoko@hotmail.com>
2018-11-17 11:05:15 +0100

    Adds style-components and theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
+import ThemeProvider from './style/ThemeProvider'
 
 class App extends Component {
   render () {
     return (
-      <div>
+      <ThemeProvider>
         <header>
           <h1>Hello</h1>
         </header>
-      </div>
+      </ThemeProvider>
     )
   }
 }
