9c603913ded409c8e53987a1f8de2965259b5e7a
Dennis Skoko <dennis.skoko@hotmail.com>
2018-11-18 16:09:17 +0100

    Adds a FullPageMessage component

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import FullPageMessage from '../ui/FullPageMessage'
 import Heading from '../ui/Heading'
 import Text from '../ui/Text'
 
 function NotFound () {
   return (
-    <section>
+    <FullPageMessage>
       <Heading centered marginTop>Page not found (;-;)</Heading>
       <Text centered>Sorry but I couldn't find the page you were looking for.</Text>
-    </section>
+    </FullPageMessage>
   )
 }
 
---
+++ client/src/ui/FullPageMessage.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const FullPageMessage = styled.section`
+  margin: ${({ theme }) => theme.spacing.xl} auto 0;
+  padding: ${({ theme }) => theme.spacing.sm};
+  max-width: 500px;
+`
+
+export default FullPageMessage
