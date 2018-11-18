18b4aec49904faf30cab2cda89c16ba5d22243ad
Dennis Skoko <dennis.skoko@hotmail.com>
2018-11-18 10:30:52 +0100

    Updates the Header to support being centered

--- client/src/ui/Heading.js
+++ client/src/ui/Heading.js
@@ -5,14 +5,18 @@ const Heading = styled.h1`
   font-family: ${({ theme }) => theme.font.family.heading};
   font-size: ${({ theme, as }) => theme.font[as].size};
   font-weight: normal;
+  text-align: ${({ centered }) => centered ? 'center' : 'left'};
+  margin-bottom: 0;
 `
 
 Heading.propTypes = {
-  as: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+  as: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
+  centered: PropTypes.bool
 }
 
 Heading.defaultProps = {
-  as: 'h1'
+  as: 'h1',
+  centered: false
 }
 
 export default Heading
