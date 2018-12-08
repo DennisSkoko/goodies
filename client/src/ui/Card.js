a1888556a0c038be937811d2cf3c7fd2d7474b02
Dennis Skoko <dennis.skoko@hotmail.com>
2018-12-08 19:00:21 +0100

    Updates the Card to be able to show hover and focus state when linkable

--- client/src/ui/Card.js
+++ client/src/ui/Card.js
@@ -1,9 +1,22 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { transitions } from 'polished'
 
 const Card = styled.div`
   padding: ${({ theme }) => theme.spacing.sm};
   margin: ${({ theme }) => `${theme.spacing.md} 0`};
   background-color: ${({ theme }) => theme.color.accent.light};
+  display: block;
+
+  ${({ theme, to, href }) => (to || href) && css`
+    border: .125rem solid ${theme.color.accent.light};
+    text-decoration: none;
+    outline: none;
+    ${transitions('border-color', theme.transition.normal)};
+
+    &:hover, &:focus {
+      border-color: ${theme.color.primary.light};
+    }
+  `}
 `
 
 export default Card
