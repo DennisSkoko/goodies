56b481e9773c7cfac7c9be926b65ec6fe9cefe59
Dennis Skoko <dennis.skoko@hotmail.com>
2018-12-08 19:01:20 +0100

    Adds padding to the Container

--- client/src/ui/Container.js
+++ client/src/ui/Container.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   max-width: ${({ theme }) => theme.container.maxWidth};
+  padding: ${({ theme }) => `0 ${theme.spacing.sm}`};
   margin: 0 auto;
 `
 
