7c51d7b0a826695a51001ababc70615499c3592e
Dennis Skoko <dennis.skoko@hotmail.com>
2018-12-21 15:13:01 +0100

    Updates TheHeader to only show the sign in link when unauthenticated

--- client/src/components/TheHeader.js
+++ client/src/components/TheHeader.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import useAuth from '../hooks/useAuth'
 import Header from '../ui/Header'
 import HeaderNav from '../ui/HeaderNav'
 import HeaderNavLink from '../ui/HeaderNavLink'
 import HeaderTitle from '../ui/HeaderTitle'
 
 function TheHeader () {
+  const { signedIn } = useAuth()
+
   return (
     <Header>
       <HeaderTitle>Goodies</HeaderTitle>
       <HeaderNav>
         <HeaderNavLink to='/' exact>Home</HeaderNavLink>
         <HeaderNavLink to='/recipe/browse'>Browse</HeaderNavLink>
-        <HeaderNavLink to='/sign-in'>Sign in</HeaderNavLink>
+        {!signedIn && <HeaderNavLink to='/sign-in'>Sign in</HeaderNavLink>}
       </HeaderNav>
     </Header>
   )
