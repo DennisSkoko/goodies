817b8e9c3d6e43fc56fcc2d09aa7016ac1b3103d
Dennis Skoko <dennis.skoko@hotmail.com>
2019-01-26 20:30:48 +0100

    Adds support for centering text and headings

--- packages/app/src/ui/Heading/Heading.js
+++ packages/app/src/ui/Heading/Heading.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import styles from './Heading.module.scss'
 
 const types = {
@@ -7,20 +8,29 @@ const types = {
   headers: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
 }
 
-function Heading ({ as: As, type, ...props }) {
+function Heading ({ as: As, type, centered, ...props }) {
   if (As === null) As = types.displays.includes(type) ? 'h1' : type
 
-  return <As className={styles[type]} {...props} />
+  return (
+    <As
+      className={classNames(styles[type], {
+        [styles.centered]: centered,
+      })}
+      {...props}
+    />
+  )
 }
 
 Heading.propTypes = {
   as: PropTypes.oneOf(types.headers),
-  type: PropTypes.oneOf([...types.displays, ...types.headers])
+  type: PropTypes.oneOf([...types.displays, ...types.headers]),
+  centered: PropTypes.bool
 }
 
 Heading.defaultProps = {
   as: null,
-  type: 'h1'
+  type: 'h1',
+  centered: false
 }
 
 export default Heading
--- packages/app/src/ui/Text/Text.js
+++ packages/app/src/ui/Text/Text.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import styles from './Text.module.scss'
 
-function Text ({ as: As, type, ...props }) {
+function Text ({ as: As, type, centered, ...props }) {
   return (
-    <As className={styles[type]} {...props} />
+    <As
+      className={classNames(styles[type], {
+        [styles.centered]: centered
+      })}
+      {...props}
+    />
   )
 }
 
 Text.propTypes = {
   as: PropTypes.oneOf(['p', 'small', 'span']),
-  type: PropTypes.oneOf(['small', 'body', 'lead'])
+  type: PropTypes.oneOf(['small', 'body', 'lead']),
+  centered: PropTypes.bool
 }
 
 Text.defaultProps = {
   as: 'p',
-  type: 'body'
+  type: 'body',
+  centered: false
 }
 
 export default Text
