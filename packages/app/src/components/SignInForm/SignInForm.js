2856700f7970bc22d3d29ae46cee67cd44a3fe22
Dennis Skoko <dennis.skoko@hotmail.com>
2019-01-28 17:20:35 +0100

    Adds a onSubmit event for SignInForm

--- packages/app/src/components/SignInForm/SignInForm.js
+++ packages/app/src/components/SignInForm/SignInForm.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import useInputState from '../../hooks/useInputState'
 import Button from '../../ui/Button'
 import Heading from '../../ui/Heading'
 import InputText from '../../ui/InputText'
 import styles from './SignInForm.module.scss'
 
-function SignInForm () {
+function SignInForm ({ onSubmit }) {
   const [email, setEmail] = useInputState('')
   const [password, setPassword] = useInputState('')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    if (onSubmit) onSubmit({ email, password })
+  }
 
   return (
     <div className={styles.wrapper}>
       <Heading type='h2' centered>Sign in</Heading>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <InputText
           label='Email'
           id='signin-email'
@@ -34,6 +40,14 @@ function SignInForm () {
       </form>
     </div>
   )
+}
+
+SignInForm.propTypes = {
+  onSubmit: PropTypes.func
+}
+
+SignInForm.defaultProps = {
+  onSubmit: null
 }
 
 export default SignInForm
