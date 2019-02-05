b764c5ee54c951e09b1018c316df59ae30f43a86
Dennis Skoko <dennis.skoko@hotmail.com>
2019-02-05 20:51:16 +0100

    Updates the InputText to also contain a error message and refactores Text

--- packages/app/src/ui/InputText/InputText.js
+++ packages/app/src/ui/InputText/InputText.js
@@ -5,29 +5,38 @@ import useToggledState from '../../hooks/useToggledState'
 import Text from '../Text'
 import styles from './InputText.module.scss'
 
-function InputText ({ label, id, value, ...props }) {
+function InputText ({ label, id, value, error, ...props }) {
   const [isFocused, toggleFocused] = useToggledState(false)
 
   return (
-    <label
-      htmlFor={id}
-      className={classNames(styles.wrapper, {
-        [styles.focused]: isFocused || value !== ''
-      })}
-    >
-      <Text className={styles.label}>
-        {label}
-      </Text>
+    <div className={styles.wrapper}>
+      <label
+        htmlFor={id}
+        className={classNames(styles.label, {
+          [styles.focused]: isFocused || value !== ''
+        })}
+      >
+        <Text className={styles.labelText}>{label}</Text>
 
-      <input
-        className={styles.input}
-        id={id}
-        onFocus={toggleFocused}
-        onBlur={toggleFocused}
-        value={value}
-        {...props}
-      />
-    </label>
+        <input
+          className={styles.input}
+          id={id}
+          onFocus={toggleFocused}
+          onBlur={toggleFocused}
+          value={value}
+          {...props}
+        />
+      </label>
+
+      <Text
+        type='small'
+        className={classNames(styles.error, {
+          [styles.active]: !!error
+        })}
+      >
+        {error || '&nbsp;'}
+      </Text>
+    </div>
   )
 }
 
@@ -35,7 +44,12 @@ InputText.propTypes = {
   label: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
+  error: PropTypes.string
+}
+
+InputText.defaultProps = {
+  error: null
 }
 
 export default InputText
--- packages/app/src/ui/Text/Text.js
+++ packages/app/src/ui/Text/Text.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import styles from './Text.module.scss'
 
-function Text ({ as: As, type, centered, ...props }) {
+function Text ({ as: As, type, centered, className, ...props }) {
   return (
     <As
-      className={classNames(styles[type], {
+      className={classNames(styles[type], className, {
         [styles.centered]: centered
       })}
       {...props}
@@ -17,13 +17,15 @@ function Text ({ as: As, type, centered, ...props }) {
 Text.propTypes = {
   as: PropTypes.oneOf(['p', 'small', 'span']),
   type: PropTypes.oneOf(['small', 'body', 'lead']),
-  centered: PropTypes.bool
+  centered: PropTypes.bool,
+  className: PropTypes.string
 }
 
 Text.defaultProps = {
   as: 'p',
   type: 'body',
-  centered: false
+  centered: false,
+  className: null
 }
 
 export default Text
