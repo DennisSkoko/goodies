8bd27816bb1d818aa59117df2f8c0641dff045d6
Dennis Skoko <dennis.skoko@hotmail.com>
2019-02-07 17:25:12 +0100

    Updates createRef() to useRef()

--- packages/app/src/ui/FadeInView/FadeInView.js
+++ packages/app/src/ui/FadeInView/FadeInView.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import useInViewDetector from '../../hooks/useInViewDetector'
 import styles from './FadeInView.module.scss'
 
 function FadeInView ({ className, ...props }) {
-  const ref = React.createRef()
+  const ref = useRef()
   const inView = useInViewDetector(ref)
 
   return (
