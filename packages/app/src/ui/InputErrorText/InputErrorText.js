6ded36d52f323ff3701c7d44c3c7ea72439ace3e
Dennis Skoko <dennis.skoko@hotmail.com>
2019-02-12 20:19:22 +0100

    Fixes issue with InputErrorText and adds helpful comments

--- packages/app/src/ui/InputErrorText/InputErrorText.js
+++ packages/app/src/ui/InputErrorText/InputErrorText.js
@@ -4,14 +4,23 @@ import Text from '../Text'
 import styles from './InputErrorText.module.scss'
 
 function InputErrorText ({ error: stagedError, ...props }) {
-  const [currentError, setCurrentError] = useState(stagedError)
+  // Because of a bug in React, this state needs to be in an object.
+  // More info: https://github.com/facebook/react/issues/14812
+  const [{ currentError }, setCurrentError] = useState({
+    currentError: stagedError
+  })
 
   if (stagedError && currentError !== stagedError) {
-    setCurrentError(stagedError)
+    setCurrentError({
+      currentError: stagedError
+    })
   }
 
   const handleTransitionEnd = (event) => {
-    if (event.propertyName === 'transform') setCurrentError(null)
+    // Dirty hack to make sure this event handler is only called once
+    if (event.propertyName === 'transform') {
+      setCurrentError({ currentError: null })
+    }
   }
 
   return (
