f0e03d2971c2d71af5f9a92f04144e9b7e2965c9
Dennis Skoko <dennis.skoko@hotmail.com>
2019-02-22 17:47:31 +0100

    Removes the workout for a bug in React

--- packages/app/src/ui/InputErrorText/InputErrorText.js
+++ packages/app/src/ui/InputErrorText/InputErrorText.js
@@ -5,22 +5,16 @@ import Text from '../Text'
 import styles from './InputErrorText.module.scss'
 
 function InputErrorText ({ error: stagedError, ...props }) {
-  // Because of a bug in React, this state needs to be in an object.
-  // More info: https://github.com/facebook/react/issues/14812
-  const [{ currentError }, setCurrentError] = useState({
-    currentError: stagedError
-  })
+  const [currentError, setCurrentError] = useState(stagedError)
 
   if (stagedError && currentError !== stagedError) {
-    setCurrentError({
-      currentError: stagedError
-    })
+    setCurrentError(stagedError)
   }
 
   const handleTransitionEnd = (event) => {
     // Dirty hack to make sure this event handler is only called once
     if (event.propertyName === 'transform') {
-      setCurrentError({ currentError: null })
+      setCurrentError(null)
     }
   }
 
