13ce339c6395a1cdaa429d7033f06ce0bcb1ec65
Dennis Skoko <dennis.skoko@hotmail.com>
2019-03-18 07:56:11 +0100

    Adds Jest

---
+++ packages/functions/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'node',
+  clearMocks: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.js'],
+  setupFiles: ['<rootDir>/test/setup.js']
+}
---
+++ packages/functions/test/setup.js
@@ -0,0 +1,2 @@
+// Removes a warning from firebase sdk regarding this variable being undefined
+process.env.FIREBASE_CONFIG = '{}'
